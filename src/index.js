7db9f77b699893dad3bee49427c1ad44e6d59485
Tom Bonnike <bonniketom@gmail.com>
2018-04-11 20:56:28 +0200

    Create express server and enable body-parser and gzip compression

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import compression from 'compression'
 
-console.log(express)
+const port = process.env.HTTP_PORT || 3001
+const server = express()
+
+server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(compression())
+
+server.listen(port)
+
+process.on('unhandledRejection', reason => {
+  console.error('UNHANDLED_REJECTION')
+  console.error(reason)
+})
