045f8c4720853658273d456b89df61052e7c4a69
Tom Bonnike <bonniketom@gmail.com>
2018-04-12 11:38:06 +0200

    Refactor server structure

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 // @flow
-import express from 'express'
-import bodyParser from 'body-parser'
-import compression from 'compression'
-import indexRoute from '@routes/index'
+import server from '@server/index'
 
 const port = process.env.HTTP_PORT || 3001
-const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use((compression(): express$Middleware))
-
-app.get('/', indexRoute)
-
+const app = server()
 app.listen(port)
 
 process.on('unhandledRejection', (reason: Error, promise: Promise<string>) => {
