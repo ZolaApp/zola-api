498e327ff5afe55e5a6077d2b03057061b60f89c
Tom Bonnike <bonniketom@gmail.com>
2018-04-14 13:38:41 +0200

    Improved overall project setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 // @flow
+import 'dotenv/config'
 import server from '@server/index'
 
 const port = process.env.HTTP_PORT || 3001
-
 const app = server()
-app.listen(port)
+app.listen(port, () => {
+  console.info(`✨  Zola API running on port ${port}. ✨`)
+})
 
 process.on('unhandledRejection', (reason: Error, promise: Promise<string>) => {
   console.error('Unhandled rejection at:', promise, 'Reason:', reason)
