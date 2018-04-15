4b0ed6936c5cd39e0e2d1d51f4cccc3d8ea0197d
Tom Bonnike <bonniketom@gmail.com>
2018-04-15 13:26:26 +0200

    Move createUser database operation to User model to be able to re-use it in seeds

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,33 @@
 // @flow
+import bcrypt from 'bcrypt'
+import database from '@server/database'
+
 export type User = {
   id: number,
   email: string,
   name: string,
   passwordHash: string
+}
+
+export type CreateUserArgs = {
+  email: string,
+  name: string,
+  passwordPlain: string
+}
+
+const createUser = async ({
+  email,
+  name,
+  passwordPlain
+}: CreateUserArgs): Promise<User> => {
+  const passwordHash: string = await bcrypt.hash(passwordPlain, 10)
+  const savedUser: Array<User> = await database('users')
+    .insert({ email, name, passwordHash })
+    .returning('*')
+
+  return savedUser[0]
+}
+
+export default {
+  createUser
 }
