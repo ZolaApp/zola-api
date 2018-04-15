2ebb175f1168702a163b67828019421a3052f4cb
Tom Bonnike <bonniketom@gmail.com>
2018-04-15 13:40:19 +0200

    Add tests for validateEmail helper

--- src/server/database/knexfile.js
+++ src/server/database/knexfile.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import dotenv from 'dotenv'
 
 // Call dotenv again in case we’re running in the context of a knex command.
@@ -9,9 +10,20 @@ export default {
 
   connection: {
     host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DATABASE,
+    database:
+      process.env.NODE_ENV === 'test'
+        ? 'zola-test'
+        : process.env.POSTGRES_DATABASE,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD
+  },
+
+  migrations: {
+    directory: path.join(__dirname, 'migrations')
+  },
+
+  seeds: {
+    directory: path.join(__dirname, `seeds/${process.env.NODE_ENV}`)
   },
 
   debug:
