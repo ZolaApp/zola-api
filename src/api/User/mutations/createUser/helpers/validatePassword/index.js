75aa88edba9db97a00e9885c01c07968591cb90f
Tom Bonnike <bonniketom@gmail.com>
2018-04-15 12:41:53 +0200

    Add tests for validatePassword helper

--- src/api/User/mutations/createUser/helpers/validatePassword/index.js
+++ src/api/User/mutations/createUser/helpers/validatePassword/index.js
@@ -1,12 +1,16 @@
 // @flow
 import zxcvbn from 'zxcvbn'
 
+const MINIMUM_SCORE = 3
+
+type PasswordValidationFeedback = {
+  warning: string,
+  suggestions: Array<string>
+}
+
 type PasswordValidation = {
   score: number,
-  feedback: {
-    warning: string,
-    suggestions: Array<string>
-  }
+  feedback: PasswordValidationFeedback
 }
 
 type PasswordValidationResult = {
@@ -14,20 +18,33 @@ type PasswordValidationResult = {
   feedback: string
 }
 
+const getFeedback = ({
+  warning,
+  suggestions
+}: PasswordValidationFeedback): string => {
+  // Zxcvbn returns two fields in their feedback: a `warning`, and `suggestions`
+  // which are an array of suggestions. Both `warning` and `suggestions` can
+  // potentially be empty. We join all of those to return a long but complete
+  // feedback message to the user.
+  const formattedSuggestions = suggestions.join(' ')
+  const feedback = warning
+    ? `${warning}. ${formattedSuggestions}`
+    : formattedSuggestions || 'Your password is not secure enough.'
+
+  return feedback
+}
+
 const validatePassword = (
   passwordPlain: string,
   userInputs: Array<string>
 ): PasswordValidationResult => {
   const passwordValidation: PasswordValidation = zxcvbn(passwordPlain)
-  const { warning, suggestions } = passwordValidation.feedback
-  const formattedSuggestions = suggestions.join(' ')
-  const feedback =
-    (warning ? `${warning}. ${formattedSuggestions}` : formattedSuggestions) ||
-    'Your password is not secure enough.'
+  const isValid = passwordValidation.score >= MINIMUM_SCORE
+  const feedback = getFeedback(passwordValidation.feedback)
 
   return {
-    isValid: passwordValidation.score > 3,
-    feedback
+    isValid,
+    feedback: isValid ? '' : feedback
   }
 }
 
