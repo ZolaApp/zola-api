67ade78ccf90f99cffbe5e7c48bd0ab503d06112
Tom Bonnike <bonniketom@gmail.com>
2018-04-16 20:21:37 +0200

    Add test script allowing to filter tests by passing the wanted test suite

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-args="$@"
+TYPES=(unit integration)
 
-npx jest --verbose --forceExit ${args}
+type="$1"
+args="${@:2}"
+
+if [[ $type == --* ]] || [ -z "$1" ]; then
+  echo "🏃‍♀️  No test type supplied, running all tests…
+  "
+elif [[ ! "${TYPES[@]}" =~ "${type}" ]]; then
+  echo "❌  Unknown test suite: \`${type}\`.
+   Available tests are: $(IFS=, ; echo "${TYPES[*]}").
+  "
+  exit 1
+fi
+
+npx jest --verbose --forceExit "${type}.spec.js" ${args}
