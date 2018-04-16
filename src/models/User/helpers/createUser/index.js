c650a505635812b6f8e1e8c94f57bc3926ab9858
Tom Bonnike <bonniketom@gmail.com>
2018-04-16 11:48:45 +0200

    Remove lower bcrypt salt rounds during testing

--- src/models/User/helpers/createUser/index.js
+++ src/models/User/helpers/createUser/index.js
@@ -3,8 +3,6 @@ import bcrypt from 'bcrypt'
 import database from '@server/database'
 import type { User } from '@models/User'
 
-const BCRYPT_SALT_ROUNDS = process.env.NODE_ENV === 'test' ? 1 : 10
-
 export type CreateUserArgs = {
   email: string,
   name: string,
@@ -16,10 +14,7 @@ const createUser = async ({
   name,
   passwordPlain
 }: CreateUserArgs): Promise<User> => {
-  const passwordHash: string = await bcrypt.hash(
-    passwordPlain,
-    BCRYPT_SALT_ROUNDS
-  )
+  const passwordHash: string = await bcrypt.hash(passwordPlain, 10)
   const savedUser: Array<User> = await database('users')
     .insert({ email, name, passwordHash })
     .returning('*')
