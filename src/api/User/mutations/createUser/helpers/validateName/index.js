a6ed629335545fd4e9ce7ad8161953ec80bc84c2
Tom Bonnike <bonniketom@gmail.com>
2018-04-16 20:51:37 +0200

    Improve validateName helper

--- src/api/User/mutations/createUser/helpers/validateName/index.js
+++ src/api/User/mutations/createUser/helpers/validateName/index.js
@@ -1,28 +1,17 @@
 // @flow
-import validator from 'validator'
-
 type NameValidationResult = {
   isValid: boolean,
   error: string
 }
 
 const validateName = (name: string): NameValidationResult => {
-  const isValid = validator.isLength(name, { min: 2, max: 30 })
-
-  if (isValid) {
-    return {
-      isValid,
-      error: ''
-    }
-  }
-
+  const isValid = name.length >= 2 && name.length <= 30
   const issue = name.length < 2 ? 'short' : 'long'
-  const error = `Your name is too ${issue}. It should be between 2 and 30 characters.`
+  const error = isValid
+    ? ''
+    : `Your name is too ${issue}. It should be between 2 and 30 characters.`
 
-  return {
-    isValid: false,
-    error
-  }
+  return { isValid, error }
 }
 
 export default validateName
