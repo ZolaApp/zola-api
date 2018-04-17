8f23cada466465525f04972fb29146df7c939e87
Tom Bonnike <bonniketom@gmail.com>
2018-04-17 22:28:30 +0200

    Add comment about test client

--- src/tests/client.js
+++ src/tests/client.js
@@ -3,6 +3,9 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import { SchemaLink } from 'apollo-link-schema'
 import schema from '@api/schema'
 
+// Creating a client with a local schema allows us to not have to start a server
+// during integration tests. We can make requests with this client instance as
+// if we were making a request directly to an API.
 export default new ApolloClient({
   cache: new InMemoryCache(),
   link: new SchemaLink({ schema })
