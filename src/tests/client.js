3eacb9997beb7aa07dc4026fb93d46896f856c64
Tom Bonnike <bonniketom@gmail.com>
2018-04-17 21:29:57 +0200

    Apply review changes

--- src/tests/client.js
+++ src/tests/client.js
@@ -1,7 +1,6 @@
 import ApolloClient from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { SchemaLink } from 'apollo-link-schema'
-
 import schema from '@api/schema'
 
 export default new ApolloClient({
