1427c0ea5a338012a9fe1ad8de71dff8ae9db742
Tom Bonnike <bonniketom@gmail.com>
2018-04-18 11:56:46 +0200

    Move errors validation from the mutation to the createUser integration test

--- src/api/User/mutations/createUser/integration.spec.js
+++ src/api/User/mutations/createUser/integration.spec.js
@@ -1,144 +1,38 @@
 import gql from 'graphql-tag'
 import testClient from '@tests/client'
 import database from '@server/database'
-import {
-  INVALID_EMAIL_ERROR,
-  EMAIL_ALREADY_IN_USE_ERROR
-} from '@models/User/createUser/validations/validateEmail'
+
+const mutation = gql`
+  mutation($email: String!, $name: String!, $password: String!) {
+    createUser(email: $email, name: $name, passwordPlain: $password) {
+      user {
+        id
+        name
+        email
+      }
+      errors {
+        field
+        message
+      }
+    }
+  }
+`
 
 describe('The `createUser` mutation', () => {
   beforeAll(async () => {
     await database.migrate.latest()
-  })
-
-  beforeEach(async () => {
     await database('users').truncate()
   })
 
-  it('should return errors if the name is not valid', async done => {
+  it('should return errors', async done => {
     const response = await testClient.mutate({
-      variables: { name: 'A', email: '', password: '' },
-      mutation: gql`
-        mutation($email: String!, $name: String!, $password: String!) {
-          createUser(email: $email, name: $name, passwordPlain: $password) {
-            user {
-              id
-              name
-              email
-            }
-            errors {
-              field
-              message
-            }
-          }
-        }
-      `
+      variables: { name: '', email: '', password: '' },
+      mutation
     })
     const { user, errors } = response.data.createUser
-    const expected = {
-      __typename: 'ValidationError',
-      field: 'name',
-      message:
-        'Your name is too short. It should be between 2 and 30 characters.'
-    }
 
     expect(user).toEqual(null)
-    expect(errors).toContainEqual(expected)
-    done()
-  })
-
-  it('should return errors if the email is not valid', async done => {
-    const response = await testClient.mutate({
-      variables: { name: '', email: 'NOT_AN_EMAIL', password: '' },
-      mutation: gql`
-        mutation($email: String!, $name: String!, $password: String!) {
-          createUser(email: $email, name: $name, passwordPlain: $password) {
-            user {
-              id
-              name
-              email
-            }
-            errors {
-              field
-              message
-            }
-          }
-        }
-      `
-    })
-    const { user, errors } = response.data.createUser
-    const expected = {
-      __typename: 'ValidationError',
-      field: 'email',
-      message: INVALID_EMAIL_ERROR
-    }
-
-    expect(user).toEqual(null)
-    expect(errors).toContainEqual(expected)
-    done()
-  })
-
-  it('should return errors if the email is already in use', async done => {
-    await database.seed.run()
-    const response = await testClient.mutate({
-      variables: { name: '', email: 'email@inuse.com', password: '' },
-      mutation: gql`
-        mutation($email: String!, $name: String!, $password: String!) {
-          createUser(email: $email, name: $name, passwordPlain: $password) {
-            user {
-              id
-              name
-              email
-            }
-            errors {
-              field
-              message
-            }
-          }
-        }
-      `
-    })
-    const { user, errors } = response.data.createUser
-    const expected = {
-      __typename: 'ValidationError',
-      field: 'email',
-      message: EMAIL_ALREADY_IN_USE_ERROR
-    }
-
-    expect(user).toEqual(null)
-    expect(errors).toContainEqual(expected)
-    done()
-  })
-
-  it('should return errors if the password is not valid', async done => {
-    const response = await testClient.mutate({
-      variables: { name: '', email: '', password: 'password' },
-      mutation: gql`
-        mutation($email: String!, $name: String!, $password: String!) {
-          createUser(email: $email, name: $name, passwordPlain: $password) {
-            user {
-              id
-              name
-              email
-            }
-            errors {
-              field
-              message
-            }
-          }
-        }
-      `
-    })
-    const { user, errors } = response.data.createUser
-    const expected = {
-      __typename: 'ValidationError',
-      field: 'password',
-      message:
-        'This is a top-10 common password. Add another word or two. Uncommon words are better.'
-    }
-
-    expect(user).toEqual(null)
-    expect(errors).toContainEqual(expected)
+    expect(errors.length).toEqual(3)
     done()
   })
 
@@ -149,21 +43,7 @@ describe('The `createUser` mutation', () => {
         email: '  FOO@BaR.cOm  ',
         password: '$uper$trongPa$$word'
       },
-      mutation: gql`
-        mutation($email: String!, $name: String!, $password: String!) {
-          createUser(email: $email, name: $name, passwordPlain: $password) {
-            user {
-              id
-              name
-              email
-            }
-            errors {
-              field
-              message
-            }
-          }
-        }
-      `
+      mutation
     })
     const { user, errors } = response.data.createUser
     const expected = {
--- src/models/User/createUser/integration.spec.js
+++ src/models/User/createUser/integration.spec.js
@@ -1,7 +1,11 @@
 import database from '@server/database'
+import {
+  INVALID_EMAIL_ERROR,
+  EMAIL_ALREADY_IN_USE_ERROR
+} from './validations/validateEmail'
 import createUser from './index'
 
-const user = {
+const validUser = {
   name: 'Foo',
   email: 'foo@bar.com',
   passwordPlain: '$uper$trongPa$$word'
@@ -16,14 +20,81 @@ describe('The User model’s `createUser` helper', () => {
     await database('users').truncate()
   })
 
+  it('should return errors if the name is not valid', async done => {
+    const { user, errors } = await createUser({
+      name: 'F',
+      email: '',
+      passwordPlain: ''
+    })
+    const expected = {
+      field: 'name',
+      message:
+        'Your name is too short. It should be between 2 and 30 characters.'
+    }
+
+    expect(user).toEqual(undefined)
+    expect(errors).toContainEqual(expected)
+    done()
+  })
+
+  it('should return errors if the email is not valid', async done => {
+    const { user, errors } = await createUser({
+      name: '',
+      email: 'NOT_AN_EMAIL',
+      passwordPlain: ''
+    })
+    const expected = {
+      field: 'email',
+      message: INVALID_EMAIL_ERROR
+    }
+
+    expect(user).toEqual(undefined)
+    expect(errors).toContainEqual(expected)
+    done()
+  })
+
+  it('should return errors if the email is already in use', async done => {
+    await database.seed.run()
+    const { user, errors } = await createUser({
+      name: '',
+      email: 'email@inuse.com',
+      passwordPlain: ''
+    })
+    const expected = {
+      field: 'email',
+      message: EMAIL_ALREADY_IN_USE_ERROR
+    }
+
+    expect(user).toEqual(undefined)
+    expect(errors).toContainEqual(expected)
+    done()
+  })
+
+  it('should return errors if the password is not valid', async done => {
+    const { user, errors } = await createUser({
+      name: '',
+      email: '',
+      passwordPlain: 'password'
+    })
+    const expected = {
+      field: 'password',
+      message:
+        'This is a top-10 common password. Add another word or two. Uncommon words are better.'
+    }
+
+    expect(user).toEqual(undefined)
+    expect(errors).toContainEqual(expected)
+    done()
+  })
+
   it('should add a user to the database and return it', async done => {
     const countBefore = await database('users').count()
-    const { user: createdUser } = await createUser(user)
+    const { user } = await createUser(validUser)
     const countAfter = await database('users').count()
 
     expect(countBefore[0].count).toEqual('0')
     expect(countAfter[0].count).toEqual('1')
-    expect(createdUser).toMatchObject({
+    expect(user).toMatchObject({
       id: 1,
       name: 'Foo',
       email: 'foo@bar.com'
@@ -32,8 +103,8 @@ describe('The User model’s `createUser` helper', () => {
   })
 
   it('should save a hash of the password and not the plain password', async done => {
-    const { user: createdUser } = await createUser(user)
-    const userKeys = Object.keys(createdUser)
+    const { user } = await createUser(validUser)
+    const userKeys = Object.keys(user)
 
     expect(userKeys).not.toContain('passwordPlain')
     expect(userKeys).toContain('passwordHash')
