a36cdc0f968e4d9ece50bf5402e4285fb1d2d784
Tom Bonnike <bonniketom@gmail.com>
2018-04-24 22:38:27 +0200

    Add comment about why we call dotenv when in the context of a knex command

--- src/server/database/knexfile.js
+++ src/server/database/knexfile.js
@@ -2,6 +2,8 @@ import path from 'path'
 import dotenv from 'dotenv'
 
 // Call dotenv again in case we’re in the context of a knex command.
+// We need to specify a path since the current working directory is not the root
+// directory.
 dotenv.config({ path: '../../../.env' })
 
 export default {
