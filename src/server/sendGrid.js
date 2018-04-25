cfe169c8108923f74a90f68efb5c73245b800805
Tom Bonnike <bonniketom@gmail.com>
2018-04-25 09:25:54 +0200

    Remove thrown error from sendGrid initialization

--- src/server/sendGrid.js
+++ src/server/sendGrid.js
@@ -1,11 +1,7 @@
 // @flow
 import sendGrid from '@sendgrid/mail'
 
-if (!process.env.SENDGRID_API_KEY) {
-  throw new Error('❌  `SENDGRID_API_KEY` environment key is missing.')
-}
-
-sendGrid.setApiKey(process.env.SENDGRID_API_KEY)
+sendGrid.setApiKey(process.env.SENDGRID_API_KEY || '')
 sendGrid.setSubstitutionWrappers('{{', '}}')
 
 export default sendGrid
