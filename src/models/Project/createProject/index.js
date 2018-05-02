6abbe9a33e3f3dee50c4b7080dc3251f3e8b9c8b
Tom Bonnike <bonniketom@gmail.com>
2018-05-02 11:25:49 +0200

    Add project description validation using the validateString helper

--- src/models/Project/createProject/index.js
+++ src/models/Project/createProject/index.js
@@ -7,6 +7,11 @@ import type { Project } from '@models/Project'
 import type { ValidationError } from '@types/ValidationError'
 
 const validateName = validateString({ type: 'name' })
+const validateDescription = validateString({
+  type: 'description',
+  minLength: 0,
+  maxLength: 255
+})
 
 export type CreateProjectArgs = {
   name: string,
@@ -26,19 +31,24 @@ const createProject = async ({
 }: CreateProjectArgs): Promise<CreateProjectResponse> => {
   const errors: Array<ValidationError> = []
   const trimmedName = name.trim()
-  const trimmedDescription = description.trim()
   const nameValidation = validateName(trimmedName)
 
   if (!nameValidation.isValid) {
     errors.push({ field: 'name', message: nameValidation.error })
   }
 
-  const slug = slugify(trimmedName, { lower: true })
+  const trimmedDescription = description.trim()
+  const descriptionValidation = validateDescription(trimmedDescription)
+
+  if (!descriptionValidation.isValid) {
+    errors.push({ field: 'description', message: descriptionValidation.error })
+  }
 
   if (errors.length) {
     return { errors }
   }
 
+  const slug = slugify(trimmedName, { lower: true })
   const savedProject: Array<Project> = await database('projects')
     .insert({ name: trimmedName, slug, description: trimmedDescription })
     .returning('*')
