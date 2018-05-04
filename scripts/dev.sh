14b84f839b7528c287262a4f137c40d128a20f6f
Tom Bonnike <bonniketom@gmail.com>
2018-05-04 10:16:14 +0200

    Keep dev.sh under 80 characters

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-BABEL_DISABLE_CACHE=1 npx nodemon --ext js,json,graphql src/index.js --exec babel-node --extensions ".js",".json",".graphql"
+BABEL_DISABLE_CACHE=1 npx nodemon --ext js,json,graphql src/index.js --exec \
+  babel-node --extensions ".js",".json",".graphql"
