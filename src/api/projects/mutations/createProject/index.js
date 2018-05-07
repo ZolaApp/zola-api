6eb76e99148951deea4b46229c2aa07d29a51ecd
Tom Bonnike <bonniketom@gmail.com>
2018-05-07 09:37:17 +0200

    Automatically retrieve ownerId from auth middleware when creating a project

--- src/api/projects/mutations/createProject/index.js
+++ src/api/projects/mutations/createProject/index.js
@@ -2,8 +2,23 @@
 import ProjectModel from '@models/Project'
 import type { CreateProjectArgs } from '@models/Project'
 
-const resolver = async (_: any, args: CreateProjectArgs) => {
-  const { errors, project } = await ProjectModel.createProject(args)
+type Context = {
+  request: express$Request
+}
+
+const resolver = async (
+  _: any,
+  args: CreateProjectArgs,
+  { request }: Context
+) => {
+  if (request.user === null) {
+    throw new Error('Please log in.')
+  }
+
+  const { errors, project } = await ProjectModel.createProject({
+    ...args,
+    ownerId: request.user.id
+  })
 
   if (errors.length > 0) {
     return { status: 'FAILURE', errors }
