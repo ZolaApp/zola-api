949dd0d9be71a2961b6c6add32f6c65ee84e389f
Tom Bonnike <bonniketom@gmail.com>
2018-05-07 09:19:50 +0200

    Fix redis setup

--- src/server/redis.js
+++ src/server/redis.js
@@ -3,10 +3,12 @@ import { promisify } from 'util'
 import redis from 'redis'
 import redisMock from 'redis-mock'
 
+const host = process.env.REDIS_HOST || '127.0.0.1'
+const port = Number(process.env.REDIS_PORT) || 6379
 const client =
   process.env.NODE_ENV === 'test'
     ? redisMock.createClient()
-    : redis.createClient(process.env.REDIS_HOST || '127.0.0.1')
+    : redis.createClient({ host, port })
 client.on('error', error => console.error('Redis error:', error))
 
 const METHODS_TO_PROMISIFY = ['set', 'get', 'del']
