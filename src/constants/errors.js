65bcaaf90022de93745dff8d1655745bf99a038e
PauuloG <paulgabriel7@gmail.com>
2018-05-15 09:47:37 +0200

    Refactored errors to use constants

---
+++ src/constants/errors.js
@@ -0,0 +1,7 @@
+// @flow
+export const AUTHENTICATION_ERROR_NO_USER =
+  'Please login to perform this action'
+export const AUTHENTICATION_ERROR_INVALID_LOGIN =
+  'Invalid authentication token provided. Please try logging in again.'
+export const AUTHENTICATION_ERROR_INVALID_CREDENTIALS =
+  'The credentials provided could not be used to authenticate you, please check your email and password'
