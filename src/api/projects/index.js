3b66552242274ed384d0bd07d5eabc2f485a3105
PauuloG <paulgabriel7@gmail.com>
2018-05-17 22:49:10 +0200

    Added getProject query

--- src/api/projects/index.js
+++ src/api/projects/index.js
@@ -2,6 +2,7 @@
 import createProject from './mutations/createProject'
 import addLocaleToProject from './mutations/addLocaleToProject'
 import getProjects from './queries/getProjects'
+import getProject from './queries/getProject'
 
 export default {
   Mutation: {
@@ -9,6 +10,7 @@ export default {
     addLocaleToProject
   },
   Query: {
-    getProjects
+    getProjects,
+    getProject
   }
 }
