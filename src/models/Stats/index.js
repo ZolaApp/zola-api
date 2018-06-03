1d23b675d68f6f2e743387d62d2722cd536204a3
PauuloG <paulgabriel7@gmail.com>
2018-06-03 15:44:47 +0200

    Removed useless if

--- src/models/Stats/index.js
+++ src/models/Stats/index.js
@@ -13,22 +13,16 @@ class Stats {
   newKeysCount: number
   completePercentage: number
   translationKeysCount: number
-  projectsCount: number
-  localesCount: number
+  projectsCount: ?number
+  localesCount: ?number
 
   constructor(args: StatsConstructor) {
     this.missingTranslationsCount = args.missingTranslationsCount
     this.newKeysCount = args.newKeysCount
     this.completePercentage = args.completePercentage
     this.translationKeysCount = args.translationKeysCount
-
-    if (args.localesCount) {
-      this.localesCount = args.localesCount
-    }
-
-    if (args.projectsCount) {
-      this.projectsCount = args.projectsCount
-    }
+    this.localesCount = args.localesCount
+    this.projectsCount = args.projectsCount
   }
 }
 
