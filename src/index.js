227495d1a3f6c05b00565acb66025fc070e2e541
PauuloG <paulgabriel7@gmail.com>
2018-06-06 16:30:40 +0200

    Securing at docker level

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import '@database/index' // Init database and Objection.
 
 const port = process.env.PORT || 3001
 const app = server()
-app.listen(Number(port), 'localhost', () => {
+app.listen(port, () => {
   console.info(`✨  Zola API running on port ${port}. ✨`)
 })
 
